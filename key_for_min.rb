62325ed114c1bb441503b02e38ad9e76590e5474
Meg Galvez <meg@oakmaine.com>
2019-04-02 14:52:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = nil 
   lowest_key = nil 
-  hash.each do |key, value|
+  name_hash.each do |key, value|
     if lowest_value || value < lowest_value
       lowest_value = value
       lowest_key = key
